2a74576f9f20a24836394eb37bbb03216e411c4d
Alex Au-Yeung <ah.auyeung@gmail.com>
2014-11-12 04:10:59 +0000

    added html5 auto validation to registartion form

--- core/config/config.inc.php
+++ core/config/config.inc.php
@@ -7,9 +7,9 @@ $database_server = 'localhost';
 $database_user = 'invest_modx';
 $database_password = 'invest+123';
 $database_connection_charset = 'utf8';
-$dbase = 'investable_live_modx';
+$dbase = 'investable_dev_modx';
 $table_prefix = 'modx_';
-$database_dsn = 'mysql:host=localhost;dbname=investable_live_modx;charset=utf8';
+$database_dsn = 'mysql:host=localhost;dbname=investable_dev_modx;charset=utf8';
 $config_options = array (
 );
 $driver_options = array (
@@ -18,32 +18,32 @@ $driver_options = array (
 $lastInstallTime = 1415604951;
 
 $site_id = 'modx54606ad77e0b12.74978315';
-$site_sessionname = 'SN543b6c0e6f7d6';
+$site_sessionname = 'SN543b6c0e6f7d6_dev';
 $https_port = '443';
 $uuid = 'bd0169d7-083f-4663-8a0a-1fd40b5c4f1f';
 
 if (!defined('MODX_CORE_PATH')) {
-    $modx_core_path= '/var/www/html/core/';
+    $modx_core_path= '/var/www/vhosts/dev.investable.vc/html/core/';
     define('MODX_CORE_PATH', $modx_core_path);
 }
 if (!defined('MODX_PROCESSORS_PATH')) {
-    $modx_processors_path= '/var/www/html/core/model/modx/processors/';
+    $modx_processors_path= '/var/www/vhosts/dev.investable.vc/html/core/model/modx/processors/';
     define('MODX_PROCESSORS_PATH', $modx_processors_path);
 }
 if (!defined('MODX_CONNECTORS_PATH')) {
-    $modx_connectors_path= '/var/www/html/connectors/';
+    $modx_connectors_path= '/var/www/vhosts/dev.investable.vc/html/connectors/';
     $modx_connectors_url= '/connectors/';
     define('MODX_CONNECTORS_PATH', $modx_connectors_path);
     define('MODX_CONNECTORS_URL', $modx_connectors_url);
 }
 if (!defined('MODX_MANAGER_PATH')) {
-    $modx_manager_path= '/var/www/html/manager/';
+    $modx_manager_path= '/var/www/vhosts/dev.investable.vc/html/manager/';
     $modx_manager_url= '/manager/';
     define('MODX_MANAGER_PATH', $modx_manager_path);
     define('MODX_MANAGER_URL', $modx_manager_url);
 }
 if (!defined('MODX_BASE_PATH')) {
-    $modx_base_path= '/var/www/html/';
+    $modx_base_path= '/var/www/vhosts/dev.investable.vc/html/';
     $modx_base_url= '/';
     define('MODX_BASE_PATH', $modx_base_path);
     define('MODX_BASE_URL', $modx_base_url);
@@ -62,7 +62,7 @@ if (!defined('MODX_HTTP_HOST')) {
         $http_host='dev.investable.vc';
         define('MODX_HTTP_HOST', $http_host);
     } else {
-        $http_host= array_key_exists('HTTP_HOST', $_SERVER) ? $_SERVER['HTTP_HOST'] : 'www.investable.vc';
+        $http_host= array_key_exists('HTTP_HOST', $_SERVER) ? $_SERVER['HTTP_HOST'] : 'dev.investable.vc';
         if ($_SERVER['SERVER_PORT'] != 80) {
             $http_host= str_replace(':' . $_SERVER['SERVER_PORT'], '', $http_host); // remove port from HTTP_HOST
         }
@@ -75,7 +75,7 @@ if (!defined('MODX_SITE_URL')) {
     define('MODX_SITE_URL', $site_url);
 }
 if (!defined('MODX_ASSETS_PATH')) {
-    $modx_assets_path= '/var/www/html/assets/';
+    $modx_assets_path= '/var/www/vhosts/dev.investable.vc/html/assets/';
     $modx_assets_url= '/assets/';
     define('MODX_ASSETS_PATH', $modx_assets_path);
     define('MODX_ASSETS_URL', $modx_assets_url);
