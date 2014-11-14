07e7c4edd472b3e44ee65b554fb05d2da3e2c5b3
Alex Au-Yeung <ah.auyeung@gmail.com>
2014-11-14 10:56:31 +0000

    added http auth and ignore list to user table

--- assets/snippets/user_table.php
+++ assets/snippets/user_table.php
@@ -6,13 +6,12 @@ if(empty($tpl)){
 	
 }
 
+$ignore_list = "16,17,376,395,397,303,224,45,173,319,259,245";
+
 $query = "SELECT a.*, u.username FROM ".$config["table_prefix"]."user_attributes a ";
 $query .= " LEFT JOIN ".$config["table_prefix"]."users u ON a.internalKey =u.id ";
 $query .= " LEFT JOIN ".$config["table_prefix"]."member_groups g ON a.internalKey = g.member";
-$query .= " WHERE a.blocked!=1 AND g.user_group=2 AND g.role=1";
-
-
-
+$query .= " WHERE a.blocked!=1 AND g.user_group=2 AND g.role=1 AND internalKey NOT IN (".$ignore_list.")";
 $query .= " ORDER BY u.id DESC";
 //return $query;
 $result = $modx->query($query);
