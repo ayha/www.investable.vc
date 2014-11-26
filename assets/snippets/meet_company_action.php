8ca84975c5d84ff791deacfad6e9f299e6dfb35d
Alex Au-Yeung <ah.auyeung@gmail.com>
2014-11-26 07:33:29 +0000

    added user email address when they commit /  meet startups

--- assets/snippets/meet_company_action.php
+++ assets/snippets/meet_company_action.php
@@ -1,15 +1,13 @@
 <?php
-$emailTo="emma@investable.vc";
+$emailTo = "emma@investable.vc";
 $emailCC = "alex@investable.vc";
 
 
-$config = $modx->getConfig();
+
 
 if(!empty($_POST)){
    foreach($_POST as $k=>$v){
-   	if($k != "pledge_amount"){
    $_POST[$k] = $modx->quote($v);
-	}
   }
   
 $user = $modx->getUser();
@@ -19,17 +17,16 @@ $profile = $user->getOne("Profile");
 $username = $profile->get("fullname");
 $useremail = $profile->get("email");
 
-$q = "INSERT INTO ext_support_company (userid, username, user_email, companyid, companyname, pledge_amount, date_sent) VALUES ($userid, '".$username."', '".$useremail."', ".$_POST["companyid"].", ".$_POST["companyname"].", ".$_POST["pledge_amount"].", '".date("Y-m-d H:i:s")."')";
+$q = "INSERT INTO ext_meet_company (userid, username, user_email, companyid, companyname, date_sent) VALUES ($userid, '".$username."', '".$useremail."', ".$_POST["companyid"].", ".$_POST["companyname"].", '".date("Y-m-d H:i:s")."')";
 $result = $modx->query($q);
 
-$tvid =13;
-$update_q = "UPDATE ".$config["table_prefix"]."site_tmplvar_contentvalues SET value=(value+".$_POST["pledge_amount"].") WHERE tmplvarid=".$tvid ." AND contentid=".$_POST["companyid"];
-$r2 = $modx->query($update_q);
+
+
 
 if(is_object($result)){
-   $subject = "New investment from investor ".$username ." to invest in ".$_POST["companyname"];
+   $subject = "New meeting request from investor ".$username." to meet with ".$_POST["companyname"];
 $from = "startups@investable.vc";
-$message = "<p>Hi,</p><p>The user <strong>".$username."</strong> (".$useremail.") from Investable.vc has expressed interest in investing USD".$_POST["pledge_amount"]." in the company <strong>".$_POST["companyname"]."</strong> on ".date("Y-m-d H:i")."</p>";
+$message = "<p>Hi,</p><p>The user <strong>".$username."</strong> (".$useremail.") from Investable.vc has expressed interest in meeting the company <strong>".$_POST["companyname"]."</strong> on ".date("Y-m-d H:i")."</p>";
 
 $modx->getService('mail', 'mail.modPHPMailer');
 $modx->mail->set(modMail::MAIL_BODY, $message);
@@ -38,7 +35,7 @@ $modx->mail->set(modMail::MAIL_FROM_NAME, "Investable.vc");
 $modx->mail->set(modMail::MAIL_SENDER, $from);;
 $modx->mail->set(modMail::MAIL_SUBJECT, $subject);
 $modx->mail->address('to', $emailTo, $emailTo);
-$modx->mail->address('to', $emailCC, $emailCC);
+$modx->mail->address('cc', $emailCC, $emailCC);
 $modx->mail->address('reply-to', $from);
 $modx->mail->setHTML(true);
 //call send mail
