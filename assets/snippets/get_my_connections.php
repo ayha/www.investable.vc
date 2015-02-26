f9077794758bc13ff00c0250b9b14684dd9b044d
Alex Au-Yeung <ah.auyeung@gmail.com>
2015-02-26 10:17:47 +0000

    corrected my connections query

--- assets/snippets/get_my_connections.php
+++ assets/snippets/get_my_connections.php
@@ -7,12 +7,14 @@ $config = $modx->getConfig();
 $prefix = $config["table_prefix"];
 $output ="";
 $user = $modx->getUser();
+
 $query = "SELECT DISTINCT u.id FROM ".$prefix."users u ";
 $query .= " LEFT JOIN ".$prefix."user_attributes a ON u.id = a.internalKey";
-$query .= " LEFT JOIN ".$prefix."member_groups g ON u.id=g.member";
-$query .= " LEFT JOIN ext_connection_requests r ON (u.id = r.from OR u.id = r.to)";
-$query .= " WHERE g.role=1 AND g.user_group = 2 AND r.request_status=1 AND a.blocked =0 AND u.active=1 AND  u.id != ".$user->get("id");
-
+//$query .= " LEFT JOIN ".$prefix."member_groups g ON u.id=g.member";
+//$query .= " LEFT JOIN ext_connection_requests r ON (u.id = r.from OR u.id = r.to) AND r.request_status=1";
+//$query .= " WHERE g.role=1 AND g.user_group = 2 AND r.request_status=1 AND a.blocked =0 AND u.active=1 AND  u.id != ".$user->get("id");
+$query .= " LEFT JOIN ext_connection_requests r ON (".$user->get("id")." = r.from OR ".$user->get("id")." = r.to)";
+$query .= " WHERE r.request_status=1 AND a.blocked =0 AND u.active=1 AND  u.id != ".$user->get("id");
 if(isset($latest) && $latest == 1){
 	
 	$query .= " ORDER BY r.connect_datetime DESC, a.fullname ASC, u.username ASC LIMIT 5";
@@ -27,20 +29,20 @@ if(isset($latest) && $latest == 1){
 
 $result = $modx->query($query);
 if (!is_object($result)) {
-   return "<p>You do not have any connections at the moment.</p>";
+   //return "<p>You do not have any connections at the moment.</p>";
 }else{
 	if($getCount == 1){
   		 return $result->rowCount();
    	}else{
 	
-   while( $row = $result->fetch(PDO::FETCH_ASSOC)){
-      $rowOutput = $modx->runSnippet("getUserById", array("uid"=>$row["id"], "tpl"=>$chunk ));
-      $output .= $rowOutput;
-
-   }
-   if($output ==""){
-      $output = "<p>You do not have any connections at the moment.</p>";
-   }
+	   while( $row = $result->fetch(PDO::FETCH_ASSOC)){
+	      $rowOutput = $modx->runSnippet("getUserById", array("uid"=>$row["id"], "tpl"=>$chunk ));
+	      $output .= $rowOutput;
+	
+	   }
+	   if($output ==""){
+	      //$output = "<p>You do not have any connections at the moment.</p>";
+	   }
 	}
 }
 
