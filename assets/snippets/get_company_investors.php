f92856bd0cb5755b5b787a702351f6f50af7f40a
Alex Au-Yeung <ah.auyeung@gmail.com>
2015-02-27 08:16:06 +0000

    make company followers and company investors static files

---
+++ assets/snippets/get_company_investors.php
@@ -0,0 +1,42 @@
+<?php
+if(empty($companyid)){
+   return false;
+}else{
+$config = $modx->getConfig();
+$user = $modx->getUser();
+
+if(empty($tpl)){
+   $tpl = "member_quick_profile_item";
+}
+
+$query = "SELECT * FROM ext_support_company WHERE companyid=".$companyid." AND username != '' AND userid != '' GROUP BY userid ORDER by username";
+
+
+
+$result = $modx->query($query);
+
+
+if(is_object($result)){
+   if($getCount == 1){
+      return $result->rowCount();
+   }else{
+      $output = "";
+      while($row = $result->fetch(PDO::FETCH_ASSOC)   ){
+
+        
+       
+        $rowOutput["uid"] = $row["userid"];
+       
+        $rowOutput["tpl"] = $tpl;
+       
+        $output .= $modx->runSnippet("getUserById", $rowOutput);
+      
+      }
+      return $output;
+   }
+   
+}else{
+   return 0;
+}
+
+}
