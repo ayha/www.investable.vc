b53a908d3c3d0559a1f4018ac89900077b634080
Alex Au-Yeung <ah.auyeung@gmail.com>
2015-02-27 02:14:44 +0000

    make approve_connection_request snippet static file

---
+++ assets/snippets/approve_connection_request.php
@@ -0,0 +1,42 @@
+<?php
+if(empty($requestid)){
+   $requestid = $modx->quote($_POST["requestid"]);
+}
+
+if(empty($hash)){
+   $hash = $modx->quote($_POST["hash"]);
+}
+
+
+if(!empty($requestid) && !empty($hash)){
+
+$config = $modx->getConfig();
+$user = $modx->getUser();
+$userid = $user->get("id");
+
+$query = "SELECT * FROM ext_connection_requests WHERE `id`=".$requestid." AND `to`='".$userid."' AND `request_hash`=".$hash;
+
+
+$result = $modx->query($query);
+if (!is_object($result)) {
+   return "There was an error sending the request. Please try again.<br />";
+}else{
+   
+    $row = $result->fetch(PDO::FETCH_ASSOC);
+   // request approved, update corresponding record and remove the rest;
+   $uq1 = "UPDATE ext_connection_requests SET request_status=1, connect_datetime='".date("Y-m-d H:i:s")."' WHERE id=".$requestid;
+  
+ $ur1 = $modx->query($uq1);
+   
+   
+   // since the 2 users are now connected, remove the redundant request items;
+   $uq2 = "DELETE FROM ext_connection_requests WHERE id != ".$requestid." AND ((`to`='".$row["to"]."' AND `from` ='".$row["from"]."') OR (`from`='".$row["to"]."' AND `to`='".$row["from"]."'))";
+   $ur2 = $modx->query($uq2);
+
+   
+   return "conneted";
+}
+
+}else{
+   return "invalid parameters";;
+}
